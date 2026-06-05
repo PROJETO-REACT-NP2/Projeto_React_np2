b662b239140fac0abf293177cbe231ee5cb0acbb
José Rodrigues <jose14jrff@gmail.com>
2026-06-04 22:42:46 -0300

    feat: Automate schema creation in Docker using init.sql

---
+++ Projeto_React_CMJS/calculadora-api/init.sql
@@ -0,0 +1,6 @@
+-- init.sql
+-- Este script é executado automaticamente pelo Docker na primeira vez que o banco sobe.
+-- Ele garante que os esquemas personalizados sejam criados automaticamente.
+
+CREATE SCHEMA IF NOT EXISTS calculadora_schema;
+CREATE SCHEMA IF NOT EXISTS bd_rsc_poo;
